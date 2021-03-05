a5bb697dbfc04d14dd6509e19f902e18e586c9dd
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-05 12:14:37 -0300

    Corrigindo nomes e apontamentos referente a import's de arquivos

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const app = require('./app');
 const dotenv = require('dotenv').config();
-const normalizePortHelper = require('./helpers/normalize-port/normalize-port.helper');
+const NormalizePortHelper = require('./shared/helpers/normalize-port/normalize-port.helper');
 
 try {
 
   if (dotenv.error) {
     throw dotenv.error;
   }
-  app.listen(normalizePortHelper(process.env.APP_PORT));
-  console.info(dotenv.parsed);
+
+  // Liga e escuta as conexões no host e na porta especificados.
+  app.listen(NormalizePortHelper(process.env.APP_PORT));
   console.info(`SERVER OK - PORT: ${process.env.APP_PORT}`);
 
 } catch (error) {
